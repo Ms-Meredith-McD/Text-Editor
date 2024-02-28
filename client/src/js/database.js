157e7097a5ad641c1efb6ab43531dff1d2b8b255
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-28 09:50:03 -0600

    first commit

---
+++ client/src/js/database.js
@@ -0,0 +1,63 @@
+/*
+  The idb package being required below provides some syntactic sugar around the methods needed to work with IndexedDB. Yes, the code you see below is actually a "prettier" version of what you would normally have to write. Be thankful. We've only been using the idb package since mid 2022. Before that students had to write this code with no helper methods. These students deserve a medal.
+*/
+import { openDB } from 'idb';
+
+// We will define a global constant for our database name so we don't mess it up anywhere
+const DB_NAME = "jate"
+
+const initdb = async () =>
+  openDB(DB_NAME, 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains(DB_NAME)) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore(DB_NAME, { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+/*
+  We need to add some code below which will take updated content and save it to IndexedDB.
+*/
+export const putDb = async (content) => {
+  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
+  
+  // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
+  const jate_var = await openDB(DB_NAME, 1);
+
+  // TODO: Now create a variable for the transaction; again, this will be referenced below.
+  const jate_tx = jate_var.transaction(DB_NAME, 'readwrite');
+
+  // TODO: Now create a variable for the store
+  const jate_store = jate_tx.objectStore(DB_NAME);
+
+  const request = jate_store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result?.value);
+};
+
+/*
+  We need to add some code below which will get all content from IndexedDB.
+*/
+export const getDb = async () => {
+  // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
+  
+  // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
+  const jate_var = await openDB(DB_NAME, 1);
+  const jate_tx = jate_var.transaction(DB_NAME, 'readonly');
+  const jate_store = jate_tx.objectStore(DB_NAME);
+
+
+  // Leave the rest as-is
+  const request = jate_store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+
+  return result?.value;
+};
+
+initdb();
---
+++ client/src/js/install.js
@@ -0,0 +1,29 @@
+const butInstall = document.getElementById('buttonInstall');
+
+/* Here we need to provide the logic for when and how to install the PWA.  */
+
+//  Keep this code as-is
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+
+// This listener fires when the user clicks the "install" button.
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  // TODO: Notice line 7 above.  window.deferredPrompt should now be set to null.
+  window.deferredPrompt = null;
+  // TODO: Notice line 8 above. We want the same line here, but the hidden value should be set to true.
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
