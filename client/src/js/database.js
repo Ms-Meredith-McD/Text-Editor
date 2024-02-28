aac07a99a22512cb0507e0280b819bc3fb23c9f9
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-28 16:02:49 -0600

    database.js updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,9 +1,6 @@
-/*
-  The idb package being required below provides some syntactic sugar around the methods needed to work with IndexedDB. Yes, the code you see below is actually a "prettier" version of what you would normally have to write. Be thankful. We've only been using the idb package since mid 2022. Before that students had to write this code with no helper methods. These students deserve a medal.
-*/
 import { openDB } from 'idb';
 
-// We will define a global constant for our database name so we don't mess it up anywhere
+// Define a global constant for our database name so we don't mess it up anywhere
 const DB_NAME = "jate"
 
 const initdb = async () =>
@@ -19,18 +16,14 @@ const initdb = async () =>
   });
 
 /*
-  We need to add some code below which will take updated content and save it to IndexedDB.
+  Add some code below which will take updated content and save it to IndexedDB.
 */
 export const putDb = async (content) => {
-  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
-  
-  // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
+  // Create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
   const jate_var = await openDB(DB_NAME, 1);
 
-  // TODO: Now create a variable for the transaction; again, this will be referenced below.
   const jate_tx = jate_var.transaction(DB_NAME, 'readwrite');
 
-  // TODO: Now create a variable for the store
   const jate_store = jate_tx.objectStore(DB_NAME);
 
   const request = jate_store.put({ id: 1, value: content });
@@ -39,18 +32,15 @@ export const putDb = async (content) => {
 };
 
 /*
-  We need to add some code below which will get all content from IndexedDB.
+Get all content from IndexedDB.
 */
 export const getDb = async () => {
-  // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
-  
-  // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
+  //same as above except 'readonly'
+
   const jate_var = await openDB(DB_NAME, 1);
   const jate_tx = jate_var.transaction(DB_NAME, 'readonly');
   const jate_store = jate_tx.objectStore(DB_NAME);
 
-
-  // Leave the rest as-is
   const request = jate_store.get(1);
   const result = await request;
   result
